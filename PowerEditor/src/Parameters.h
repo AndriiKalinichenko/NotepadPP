af7b8e148cf16dd296dde05ba08226cada8e0f31
Andriy Kalinichenko <kalinichenkoandrew@gmail.com>
2014-07-16 17:08:24 +0300

    Separated structs Session, sessionFileInfo and Position to individual file

--- PowerEditor/src/Parameters.h
+++ PowerEditor/src/Parameters.h
@@ -69,6 +69,9 @@
 #include "dpiManager.h"
 #endif //DPIMANAGER_H
 
+#ifndef SESSION_H
+#include "Session.h"
+#endif //SESSION_H
 
 #include <tchar.h>
 
@@ -138,47 +141,6 @@ const TCHAR notepadStyleFile[] = TEXT("asNotepad.xml");
 
 void cutString(const TCHAR *str2cut, vector<generic_string> & patternVect);
 
-
-struct Position
-{ 
-	int _firstVisibleLine;
-	int _startPos;
-	int _endPos;
-	int _xOffset;
-	int _selMode;
-	int _scrollWidth;
-	Position() : _firstVisibleLine(0), _startPos(0), _endPos(0), _xOffset(0), _scrollWidth(1), _selMode(0) {};
-};
-
-struct sessionFileInfo : public Position {
-	sessionFileInfo(const TCHAR *fn, const TCHAR *ln, int encoding, Position pos, const TCHAR *backupFilePath, int originalFileLastModifTimestamp) : 
-		_encoding(encoding), Position(pos), _originalFileLastModifTimestamp(originalFileLastModifTimestamp) {
-		if (fn) _fileName = fn;
-		if (ln)	_langName = ln;
-		if (backupFilePath) _backupFilePath = backupFilePath;
-	};
-
-	sessionFileInfo(generic_string fn) : _fileName(fn), _encoding(-1){};
-	
-	generic_string _fileName;
-	generic_string	_langName;
-	vector<size_t> _marks;
-	vector<size_t> _foldStates;
-	int	_encoding;
-
-	generic_string _backupFilePath;
-	time_t _originalFileLastModifTimestamp;
-};
-
-struct Session {
-	size_t nbMainFiles() const {return _mainViewFiles.size();};
-	size_t nbSubFiles() const {return _subViewFiles.size();};
-	size_t _activeView;
-	size_t _activeMainIndex;
-	size_t _activeSubIndex;
-	vector<sessionFileInfo> _mainViewFiles;
-	vector<sessionFileInfo> _subViewFiles;
-};
 
 struct CmdLineParams {
 	bool _isNoPlugin;
